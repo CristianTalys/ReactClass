b7b88a209449327e0e52e1627cbd38b581c5ca1b
Cristian Talys Braga Borges <contato.jinkai@gmail.com>
2024-07-26 23:18:48 -0300

    Fim do modulo de CSS no react

--- 4_CSS_NO_REACT/REACT_CSS/src/components/CssModue.jsx
+++ 4_CSS_NO_REACT/REACT_CSS/src/components/CssModue.jsx
@@ -1,16 +1,18 @@
-import classes from "./CssModue.module.css";
+import myClass from "./CssModue.module.css";
 
 const CssModue = () => {
   return (
-    <div className="content">
-        <h2 className="subtitle">CSS Modules</h2>
-        <ul>
-            <li>CSS Modules permite deixar o CSS <strong>scoped</strong>;</li>
-            <li>Ou seja, ele só funciona para o componente em questão;</li>
-            <li>O nome do arquivo fica: <strong>Component.module.css</strong>;</li>
-            <li>É necessário fazer a importação também;</li>
-        </ul>
-        <div className="caixaTeste"></div>
+    <div className={myClass.myClass}>
+      <div className="content">
+          <h2 className="subtitle">CSS Modules</h2>
+          <ul>
+              <li>CSS Modules permite deixar o CSS <strong>scoped</strong>;</li>
+              <li>Ou seja, ele só funciona para o componente em questão;</li>
+              <li>O nome do arquivo fica: <strong>Component.module.css</strong>;</li>
+              <li>É necessário fazer a importação também;</li>
+          </ul>
+          <div className="caixaTeste"></div>
+      </div>
     </div>
   )
 }
